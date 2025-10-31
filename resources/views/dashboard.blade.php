4f08737757995dc4f6e08accc3001c9fdce74a41
Your Name <you@example.com>
2025-10-31 12:35:26 +0700

    update wire navigate dashboard menus

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,30 +5,30 @@
             <h2 class="mx-auto">Stats Overview<span class="text-red-500 ml-4">not yet</span></h2>
         </div>
         <div class="grid auto-rows-min gap-4 grid-cols-2 md:grid-cols-3">
-            <a href="{{ route('pos') }}"
+            <a wire:navigate href="{{ route('pos') }}"
                 class="flex items-center aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <h2 class="mx-auto">Kasir<span class="text-blue-500 ml-4">ongoing</span></h2>
             </a>
-            <a href="{{ route('products') }}"
+            <a wire:navigate href="{{ route('products') }}"
                 class="flex items-center aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <h2 class="mx-auto">Produk<span class="text-blue-500 ml-4">ongoing</span></h2>
             </a>
-            <div
+            <a
                 class="flex items-center aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <h2 class="mx-auto">Stock Opname<span class="text-red-500 ml-4">not yet</span></h2>
-            </div>
-            <a href="{{ route('employees') }}"
+            </a>
+            <a wire:navigate href="{{ route('employees') }}"
                 class="flex items-center aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <h2 class="mx-auto">HR & Payroll<span class="text-red-500 ml-4">not yet</span></h2>
             </a>
-            <div
+            <a
                 class="flex items-center aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <h2 class="mx-auto">Statistik<span class="text-red-500 ml-4">not yet</span></h2>
-            </div>
-            <div
+            </a>
+            <a wire:navigate href="{{ route('orders') }}"
                 class="flex items-center aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <h2 class="mx-auto">Orders<span class="text-red-500 ml-4">not yet</span></h2>
-            </div>
+            </a>
         </div>
     </div>
 </x-layouts.app>
